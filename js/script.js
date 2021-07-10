234b56572740a9fa2a66578cc318308bf0a612f1
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-07-10 20:39:24 +0000

    Removed search-icon click at script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
 
-    // Show or Hide Search Box when Search Icon is Clicked
-    $("#search-icon").click(function() {
-        $('.search-box').show();
-        $(this).hide();
-    });
-
-   
     // Get Article when Button is Clicked
     $('.btn').click(function() {
         let articleTerm = $('#inputbox').val();
